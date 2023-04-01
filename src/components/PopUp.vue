259256fc91b67d428aecd91e68ea3943e1df1e74
Aditya Jirafe <adityajirafe@Adityas-MacBook-Pro-2.local>
2023-04-02 00:52:07 +0800

    Add Leaves functionality

---
+++ src/components/PopUp.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="popup-wrapper" v-show="modelValue">
+    <div class="popup" v-show="modelValue">
+      <div class="popup-inner">
+        <slot id="message"></slot>
+        <button class="close-popup" @click="$emit('update:modelValue', false)">OK</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.popup-wrapper {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  z-index: 1000;
+}
+
+.popup {
+  position: absolute;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  padding: 30px 50px;
+  transform: translate(-50%, -50%);
+}
+.popup-inner {
+  width: 330px;
+  justify-content: center;
+}
+
+.close-popup {
+  margin-top: 10px;
+  cursor: pointer;
+  background-color: #2c5b94;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  padding: 10px 20px;
+  font-size: 14px;
+  font-weight: bold;
+}
+#message {
+  text-align: center;
+}
+</style>
