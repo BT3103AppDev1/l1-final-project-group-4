c3e0867b4ba96a95350909a2db1bf68b40673abf
ca-leb <wolbelac@gmail.com>
2023-04-16 22:21:04 +0800

    fix dog profile pic bug

--- src/components/AppointmentDetails.vue
+++ src/components/AppointmentDetails.vue
@@ -142,7 +142,7 @@ export default {
         await getDownloadURL(storageref(storage, userEmail + '-' + pet + '.png')).then((url) => {
           img.setAttribute('src', url);
         });
-        img.id = 'dog-img';
+        img.id = 'appointments-dog-img';
         div1.appendChild(img);
 
         let div2 = document.createElement('div');
@@ -352,7 +352,7 @@ export default {
   margin-left: 3em;
 }
 
-#dog-img {
+#appointments-dog-img {
   height: 200px;
   width: 200px;
   border-radius: 50%;
@@ -360,7 +360,7 @@ export default {
 }
 
 @media (max-width: 1040px) {
-  #dog-img {
+  #appointments-dog-img {
     display: none;
   }
 }
